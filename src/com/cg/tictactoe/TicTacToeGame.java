88c65b31ab05c7766e32c3534af2a507d459b554
NCSaketh <nc.saketh1@gmail.com>
2020-10-01 10:46:28 +0530

    Player chooses X or O

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,22 +1,48 @@
 package com.cg.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
+
 	public static void main(String[] args) {
-		
+       // Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the TicTacToe Game");
-        char board[]= createBoard();
+		char board[] = createBoard();
+		inputXorO();
+	}
+
+	/**
+	 * Creating Tic Tac Toe board
+	 * @return
+	 */
+	public static char[] createBoard() {
+
+		char[] board = new char[10];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
 	}
 	
-	
-	public static char[] createBoard() {
+	public static void inputXorO() {
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Choose X or O");
+		String input = sc.next();
 		
-		char[] board = new char[10];		
-		for(int i=0;i<board.length;i++)
-		{
-			board[i]= ' ';
-		}		
-		return board;
+		if(input.equals("X") || input.equals("x")) {			
+			System.out.println("Letter of the player is X");
+	    	System.out.println("Letter of the computer is O");
+		}
+	   	else if(input.equals("O") || input.equals("o"))  {	   		
+	   		System.out.println("Letter of the player is O");
+		    System.out.println("Letter of the computer is X");
+	    }
+	   	else {
+	   		System.out.println("Invalid input");
+	   	}
+	
+		
 	}
 
 }
