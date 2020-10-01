7ad2e8767db12a47b48fdd2e663fea2432bd2426
NCSaketh <nc.saketh1@gmail.com>
2020-10-01 09:48:41 +0530

    Updating UC1 Creating Board

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,22 +1,21 @@
-package TicTacToe;
+package com.cg.tictactoe;
 
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to the TicTacToe Game");
-        char board[]= CreateBoard();
+        char board[]= createBoard();
 	}
 	
-	public static char[] CreateBoard() {
+	
+	public static char[] createBoard() {
 		
-		char[] board = new char[10];
-		
-		for(int i=0;i<10;i++)
+		char[] board = new char[10];		
+		for(int i=0;i<board.length;i++)
 		{
 			board[i]= ' ';
-		}
-		
+		}		
 		return board;
 	}
 
