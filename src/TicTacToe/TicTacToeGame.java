e8141ad60f3912fd5541304e91ebe20a92247448
NCSaketh <nc.saketh1@gmail.com>
2020-10-01 09:31:01 +0530

    UC1:Creating Board

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -3,8 +3,21 @@ package TicTacToe;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Welcome to the TicTacToe Game");
+        char board[]= CreateBoard();
+	}
+	
+	public static char[] CreateBoard() {
+		
+		char[] board = new char[10];
+		
+		for(int i=0;i<10;i++)
+		{
+			board[i]= ' ';
+		}
+		
+		return board;
 	}
 
 }
