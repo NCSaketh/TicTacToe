22b04767322d58dc5050eb1e92c6bdb8d17cd9f9
NCSaketh <nc.saketh1@gmail.com>
2020-10-04 21:02:04 +0530

    PC move if it can win

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -10,20 +10,18 @@ public class TicTacToeGame {
 	public static final int HEAD = 0;
 	public static final int TAIL = 1;
 	static char userSymbol = inputXorO();
-	static char pcSymbol = (userSymbol == 'X') ? 'O' : 'X'; 
+	static char pcSymbol = (userSymbol == 'X') ? 'O' : 'X';
 	static int index = 0;
 	static char input = ' ';
 
-	
-
 	public static void main(String[] args) {
-		
+
 		Tossing();
 		board = createBoard();
-		//inputXorO();
+		// inputXorO();
 		showBoard(board);
 		userMove(board, input);
-		//makeMove(board, index, input);
+		// makeMove(board, index, input);
 
 	}
 
@@ -67,29 +65,29 @@ public class TicTacToeGame {
 	// UC4 :Selecting Index and ensuring the index is free
 	private static void userMove(char board[], char input) {
 
-		//do {
-			System.out.println("select index between 1-9");
-			index = sc.nextInt();
-			sc.nextLine();
+		// do {
+		System.out.println("select index between 1-9");
+		index = sc.nextInt();
+		sc.nextLine();
 
-			if (isFreeIndex(board, index) == false) {
+		if (isFreeIndex(board, index) == false) {
 
-				System.out.println("Index already filled");
-				userMove(board, input);
-			} else if (index < 1 || index > 9) {
-				System.out.println("Invalid Index choice! Enter again");
-				userMove(board, input);
-			} else {
-				board[index] = userSymbol;
-				showBoard(board);
-			}
+			System.out.println("Index already filled");
+			userMove(board, input);
+		} else if (index < 1 || index > 9) {
+			System.out.println("Invalid Index choice! Enter again");
+			userMove(board, input);
+		} else {
+			board[index] = userSymbol;
+			showBoard(board);
+		}
 
-		//} while (board[index] != ' ');
+		// } while (board[index] != ' ');
 
-		//return showBoard(board);
+		// return showBoard(board);
 	}
 
-	private static boolean isFreeIndex( char board[], int index) {
+	private static boolean isFreeIndex(char board[], int index) {
 		if (board[index] == 'X' || board[index] == 'O')
 			return false;
 		else
@@ -104,41 +102,94 @@ public class TicTacToeGame {
 	}
 
 	// UC6 : Toss to check who starts first
-	private static int  Tossing() {
+	private static int Tossing() {
 		int toss = (int) Math.floor(Math.random() * 2);
-		if(toss == 0)
+		if (toss == 0)
 			System.out.println("User won the toss");
 		else
 			System.out.println("Computer won the toss");
 		return toss;
 	}
-	
-	private static int gameStatus(char board[], char Symbol)
-	{
-		if((board[1]==Symbol && board[2]==Symbol && board[3]==Symbol) || (board[4]==Symbol && board[5]==Symbol && board[6]==Symbol)
-		|| (board[7]==Symbol && board[8]==Symbol && board[9]==Symbol) || (board[1]==Symbol && board[4]==Symbol && board[7]==Symbol)
-		|| (board[2]==Symbol && board[5]==Symbol && board[8]==Symbol) || (board[3]==Symbol && board[6]==Symbol && board[9]==Symbol)
-		|| (board[1]==Symbol && board[5]==Symbol && board[9]==Symbol)|| (board[3]==Symbol && board[5]==Symbol && board[7]==Symbol))
-		{
-			if(Symbol == userSymbol)
+
+	//Checking if anyone has won or game tied or change turn
+	private static int gameStatus(char board[], char Symbol) {
+		if ((board[1] == Symbol && board[2] == Symbol && board[3] == Symbol)
+				|| (board[4] == Symbol && board[5] == Symbol && board[6] == Symbol)
+				|| (board[7] == Symbol && board[8] == Symbol && board[9] == Symbol)
+				|| (board[1] == Symbol && board[4] == Symbol && board[7] == Symbol)
+				|| (board[2] == Symbol && board[5] == Symbol && board[8] == Symbol)
+				|| (board[3] == Symbol && board[6] == Symbol && board[9] == Symbol)
+				|| (board[1] == Symbol && board[5] == Symbol && board[9] == Symbol)
+				|| (board[3] == Symbol && board[5] == Symbol && board[7] == Symbol)) {
+			if (Symbol == userSymbol)
 				System.out.println("User has won the game.");
 			else
 				System.out.println("Computer has won the game.");
-			return -1; 
+			return -1;
 		}
-		for(int index = 1; index<board.length; index++)
-			if(isFreeIndex(board,index) == true)
-			{
-				if(Symbol == userSymbol)
-					System.out.println("Conputer's turn.");
+		for (int index = 1; index < board.length; index++)
+			if (isFreeIndex(board, index) == true) {
+				if (Symbol == userSymbol)
+					System.out.println("Computer's turn.");
 				else
 					System.out.println("User's turn.");
 				return 0;
 			}
-		System.out.println("The game has ended with a tie.");
+		System.out.println("Game tied");
 		showBoard(board);
 		return 1;
-		
+	}
+	private static void pcMove(char board[]) {
+		// make winning move
+		if (board[1] == pcSymbol && board[2] == pcSymbol && board[3] == ' ')
+			board[3] = pcSymbol;
+		else if (board[1] == pcSymbol && board[2] == ' ' && board[3] == pcSymbol)
+			board[2] = pcSymbol;
+		else if (board[1] == ' ' && board[2] == pcSymbol && board[3] == pcSymbol)
+			board[1] = pcSymbol;
+		else if (board[4] == pcSymbol && board[5] == pcSymbol && board[6] == ' ')
+			board[6] = pcSymbol;
+		else if (board[4] == pcSymbol && board[5] == ' ' && board[6] == pcSymbol)
+			board[5] = pcSymbol;
+		else if (board[4] == ' ' && board[5] == pcSymbol && board[6] == pcSymbol)
+			board[4] = pcSymbol;
+		else if (board[7] == pcSymbol && board[8] == pcSymbol && board[9] == ' ')
+			board[9] = pcSymbol;
+		else if (board[7] == pcSymbol && board[8] == ' ' && board[9] == pcSymbol)
+			board[8] = pcSymbol;
+		else if (board[7] == ' ' && board[8] == pcSymbol && board[9] == pcSymbol)
+			board[7] = pcSymbol;
+		else if (board[1] == pcSymbol && board[4] == pcSymbol && board[7] == ' ')
+			board[7] = pcSymbol;
+		else if (board[1] == pcSymbol && board[4] == ' ' && board[7] == pcSymbol)
+			board[4] = pcSymbol;
+		else if (board[1] == ' ' && board[4] == pcSymbol && board[7] == pcSymbol)
+			board[1] = pcSymbol;
+		else if (board[2] == pcSymbol && board[5] == pcSymbol && board[8] == ' ')
+			board[8] = pcSymbol;
+		else if (board[2] == pcSymbol && board[5] == ' ' && board[8] == pcSymbol)
+			board[5] = pcSymbol;
+		else if (board[2] == ' ' && board[5] == pcSymbol && board[8] == pcSymbol)
+			board[2] = pcSymbol;
+		else if (board[3] == pcSymbol && board[6] == pcSymbol && board[9] == ' ')
+			board[9] = pcSymbol;
+		else if (board[3] == pcSymbol && board[6] == ' ' && board[9] == pcSymbol)
+			board[6] = pcSymbol;
+		else if (board[3] == ' ' && board[6] == pcSymbol && board[9] == pcSymbol)
+			board[3] = pcSymbol;
+		else if (board[1] == pcSymbol && board[5] == pcSymbol && board[9] == ' ')
+			board[9] = pcSymbol;
+		else if (board[1] == pcSymbol && board[5] == ' ' && board[9] == pcSymbol)
+			board[5] = pcSymbol;
+		else if (board[1] == ' ' && board[5] == pcSymbol && board[9] == pcSymbol)
+			board[1] = pcSymbol;
+		else if (board[3] == pcSymbol && board[5] == pcSymbol && board[7] == ' ')
+			board[7] = pcSymbol;
+		else if (board[3] == pcSymbol && board[5] == ' ' && board[7] == pcSymbol)
+			board[5] = pcSymbol;
+		else if (board[3] == ' ' && board[5] == pcSymbol && board[7] == pcSymbol)
+			board[3] = pcSymbol;
+
 	}
 
 	
